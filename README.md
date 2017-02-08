25afd35473162ef010f9da8b58bbe8038d4790f1
Don Rouse <Don@HotelDon.com>
2017-02-08 13:07:47 -0800

    Change comment system from HTML Comment Box to Isso

--- README.md
+++ README.md
@@ -1,18 +1,30 @@
 # GathererComments
-Adds a new comment section to the MTG Gatherer website to each card page (the current comment system is disabled)
+Adds a new comment box to the bottom of card pages on the MTG Gatherer website (useful since the current comment system is disabled).
 
-Installation Instructions: 
+###Installation Instructions (Chrome):
 
-1) Download GathererComments.zip
+1. Download [GathererComments.crx](https://github.com/HotelDon/GathererComments/raw/master/GathererComments.crx)
+2. Go to Settings (the button with three vertical dots) -> Extensions.
+3. Drag GathererComments.crx into the extensions window
+4. Select "Add Extension" to add extension.
 
-2) Unzip GathererComments.zip into it's own folder. 
+###Installation Instruction (Firefox):
 
-3) Go to settings -> extensions, and in the top right check "developer mode"
+1. Download [GathererComments.zip](https://github.com/HotelDon/GathererComments/raw/master/GathererComments.zip)
+2. Unzip GathererComments.zip into its own folder, somewhere you can access it.
+3. Open Menu (the hamburger button) -> Add-ons -> Settings (the sprocket wheel by the search bar) -> Debug add-ons -> Load Temporary Add-on
+4. Go to the folder you unzipped the file into, select manifest.json, and select open. 
+5. You're all set - however, you will need to do this every time you restart Firefox, as Firefox won't let you permanently install unpublished extensions.
 
-4) Then select "Load Unpacked Extension", and select the folder you unzipped GathererComments.zip into. 
+###Installation Instructions (Edge)
 
-Now, just visit a Gatherer card details page, and it should hopefully work!
+1. Sorry, you're on your own with this one.
 
-NOTE: The comment section disappears if the window is shrinked beyond a certain size - yes, I realize this isn't ideal, especially for those that have smaller screens. I'm working on a better fix for this.
+###Additional Notes:
 
-DOUBLE NOTE: The comment system I currently use, HTML Comment Box, is also not ideal. If this project gathers any real interest, I will likely upgrade to something else. 
+1. The rules are simple - if your comment is deleted, you broke the rules. The easiest ways to get your comment deleted is to be a jerk, or to abuse the comment box with unicode/whitespace nonsense.
+2. Name is mandatory, website and email are not. However, using an email address makes it easier for me to contact you if something happens. The website field is purely for vanity (clicking on your name will lead people to your website).   
+3. Each card gets its own comment thread, regardless of the number of printings. This is a change from the original Gatherer comments, where each printing got it's own set of comments.
+4. The comment box will disappear when the screen shrinks to below 750px, which is the threshold at which The Gatherer decides you're on a mobile device and switches layouts. Since this is a desktop browser extension, and the comments section looks really weird squished down to mobile size, it's easier just to hide it.
+5. The anti-spam features on the comment system I'm using aren't exactly robust, but since these comment boxes aren't just floating around publicly on a web page, it should hopefully be less of an issue. However, if you notice anything weird, please send me a message and let me know about it. 
+6. I haven't done anything fancy enough to preclude this from working on IE/Edge, but you're on your own for that one. 
--- commentLoader.js
+++ commentLoader.js
@@ -1,76 +1,43 @@
 let cardName = document.getElementsByClassName("value")[1].innerHTML.replace(/\s|'|"|,/g, "");
-let pageURL = (window.location.href.split("Details"))[0] + cardName + "/";
+let pageURL = (window.location.href.split("gatherer.wizards.com"))[1];
 
-let modKey = "&mod=%241%24wq1rdBcg%24NVvSZ6qsTGzqKD9aCvQ.X%2F";
-let optKey = "&opts=16798&num=10&ts=1485771478900";
+let jsURL = "https://gatherer.jank.zone/js/embed.min.js";
 
-let htmlURL = "https://www.htmlcommentbox.com/jread?page=" + pageURL + modKey + optKey;
+let scriptNode = document.createElement("script");
+let htmlNode = document.createElement("section");
 
-let xhr = new XMLHttpRequest();
-xhr.open('GET', htmlURL, true);
-xhr.onreadystatechange = responseCallback;
-xhr.send();
+scriptNode.setAttribute("data-isso", "https://gatherer.jank.zone");
+scriptNode.setAttribute("data-isso-css", "false");
+scriptNode.setAttribute("data-isso-avatar-bg", "transparent");
+scriptNode.setAttribute("data-isso-require-author", "true");
+scriptNode.setAttribute("src", jsURL);
+
+htmlNode.setAttribute("id", "isso-thread");
+htmlNode.setAttribute("data-title", cardName);
+htmlNode.setAttribute("data-isso-id", cardName);
+
+let insertLocation = document.getElementsByClassName("smallGreyBorder")[1];
+insertLocation.insertBefore(htmlNode, null);
+insertLocation.insertBefore(scriptNode, null);
 
 window.addEventListener("resize", resizeFunction);
 
-function responseCallback()
-{
-    if(xhr.readyState == XMLHttpRequest.DONE && xhr.status == 200)
-    {
-        let mainContainerDIV = document.getElementById("ctl00_ctl00_ctl00_MainContainer");
-        let htmlNode = document.createElement("div");
-        let anchorTag = document.createElement("a");
-        let scriptNode = document.createElement("script");
-        
-        mainContainerDIV.setAttribute("style", "width: initial");
-        
-        anchorTag.setAttribute("href", "//www.htmlcommentbox.com");
-        anchorTag.text = "Comment Form";
-        
-        htmlNode.setAttribute("id", "HCB_comment_box");
-        
-        htmlNode.appendChild(anchorTag);
-        htmlNode.appendChild(document.createTextNode(" is loading comments..."));
-        htmlNode.setAttribute("class", "commentLoader");
-        
-        scriptNode.innerHTML = modifyResponseText(xhr.responseText);
-        
-        document.body.appendChild(htmlNode);
-        document.body.appendChild(scriptNode);
-    }
-}
-
-function modifyResponseText(text)
-{
-    return text.replace("hcb_user.PAGE", "\"" + pageURL + "\"");
-}
+resizeFunction();
 
 function resizeFunction()
 {
-    let mainContainerDIV = document.getElementById("ctl00_ctl00_ctl00_MainContainer");
-    let commentBox = document.getElementById("HCB_comment_box");
-    
-    if(window.innerWidth < "1160")
+    if (window.innerWidth < "750")
     {
-        //console.log("Too Small");
-        if(mainContainerDIV.style.display !== "none" || commentBox.style.width !== "750px")
+        if (document.getElementById("isso-thread").style.display !== "none")
         {
-            commentBox.setAttribute("style", "display: none");
-            mainContainerDIV.setAttribute("style", "width = 750px");
+            document.getElementById("isso-thread").setAttribute("style", "display: none");
         }
     }
     else
     {
-        if(getComputedStyle(commentBox, null).display === "none" || mainContainerDIV.style.width === "750px")
+        if(document.getElementById("isso-thread").style.display === "none")
         {
-            mainContainerDIV.setAttribute("style", "width: initial");
-            commentBox.removeAttribute("style");
+            document.getElementById("isso-thread").removeAttribute("style");
         }
     }
 }
-
-// Don't worry about this. You didn't see anything. 
-//let insertLocation = document.getElementsByClassName("smallGreyBorder")[2];
-//insertLocation.insertBefore(newNode, null);
-
-
